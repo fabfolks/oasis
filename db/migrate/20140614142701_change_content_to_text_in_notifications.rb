f7ddb129b23afcae4f707e6c2c97ce0c6268d8b3
Sumanth Nagadavalli <sumanth.nagadavalli@rackspace.com>
2014-06-14 19:58:51 +0530

    Sumanth
    Changed content to text type in notifications

---
+++ db/migrate/20140614142701_change_content_to_text_in_notifications.rb
@@ -0,0 +1,9 @@
+class ChangeContentToTextInNotifications < ActiveRecord::Migration
+  def up
+    change_column :notifications, :content, :text
+  end
+
+  def down
+    change_column :notifications, :content, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140614124901) do
+ActiveRecord::Schema.define(:version => 20140614142701) do
 
   create_table "houses", :force => true do |t|
     t.string   "name"
@@ -52,9 +52,9 @@ ActiveRecord::Schema.define(:version => 20140614124901) do
   create_table "notifications", :force => true do |t|
     t.integer  "member_id"
     t.string   "title"
-    t.string   "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.text     "content",    :limit => 255
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
 end
